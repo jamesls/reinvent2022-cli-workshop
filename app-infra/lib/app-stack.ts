9c8144997e88de6f57b44cbfdbe49ccd35fba6bf
James Saryerwinnie <js@jamesls.com>
2022-10-10 12:55:44 -0400

    Plumb ddb table through

--- app-infra/lib/app-stack.ts
+++ app-infra/lib/app-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ddb from "aws-cdk-lib/aws-dynamodb";
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
 
@@ -15,8 +16,19 @@ export class AppStack extends cdk.Stack {
     const cluster = new ecs.Cluster(this, "CLIWorkshopCluster", {
       vpc: vpc
     });
+    const table = new ddb.Table(this, "APPTable", {
+      partitionKey: {
+        name: "PK",
+        type: ddb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "SK",
+        type: ddb.AttributeType.STRING,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+    });
 
-    // Create a load-balanced Fargate service and make it public
     const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "AWSCLIWorkshopDemoApp", {
       cluster: cluster,
       cpu: 512,
@@ -25,12 +37,15 @@ export class AppStack extends cdk.Stack {
         image: ecs.ContainerImage.fromAsset('../src'),
         containerPort: 5000,
 	environment: {
-          'MYAPP_FOO': 'BAR',
+          'APP_TABLE_NAME': table.tableName,
 	},
       },
       memoryLimitMiB: 2048,
       publicLoadBalancer: true,
     });
+    if (service.taskDefinition.executionRole !== undefined) {
+      table.grantReadWriteData(service.taskDefinition.executionRole);
+    }
     service.targetGroup.setAttribute('deregistration_delay.timeout_seconds', '10')
     service.targetGroup.configureHealthCheck({
       interval: cdk.Duration.seconds(5),
--- src/main.py
+++ src/main.py
@@ -4,19 +4,21 @@ from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length, Regexp
-from wtforms.fields import DecimalField, StringField, PasswordField, BooleanField, SubmitField, URLField
+from wtforms.fields import IntegerField, StringField, PasswordField
+from wtforms.fields import BooleanField, SubmitField, URLField
 from wtforms.fields import FloatField, TextAreaField
 
 
 app = Flask(__name__)
 app.secret_key = 'dev'
 bootstrap = Bootstrap5(app)
+APP_TABLE_NAME = os.environ.get('APP_TABLE_NAME', '')
 
 
 class ReviewForm(FlaskForm):
     product_name = StringField('Product Name', validators=[DataRequired(), Length(1, 50)])
     product_link = URLField()
-    rating = DecimalField()
+    rating = IntegerField()
     review_text = TextAreaField()
     submit = SubmitField()
 
@@ -36,6 +38,7 @@ def index():
     return render_template(
         'index.html',
         reviews=reviews,
+        table_name=APP_TABLE_NAME,
     )
 
 
