ef2054b8f72983bbd2dbd955f78f3e803b21452e
James Saryerwinnie <js@jamesls.com>
2022-10-20 17:53:43 -0400

    Remove prod env for now
    Speeds up initial deployment and not sure we'll need deployment
    pipeline stuff for now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,9 @@ copilot app init cli-workshop-app
 # touch the environment manifest file if it already exists,
 # we just need to specify it so it doesn't prompt us.
 copilot env init --name dev --profile ${PROFILE_NAME} --default-config
-copilot env init --name prod --profile ${PROFILE_NAME} --default-config
+#copilot env init --name prod --profile ${PROFILE_NAME} --default-config
 copilot env deploy --name dev
-copilot env deploy --name prod
+#copilot env deploy --name prod
 copilot svc init --name cli-workshop-svc
 copilot svc deploy --name cli-workshop-svc --env dev
 copilot svc show --name cli-workshop-svc --json | jp -u routes[0].url > dev-endpoint-url
